fcd6d5168b8b7d48c4142eacdd2a7cbbacc64956
Drinoff <drinoff@gmail.com>
2022-03-11 13:52:47 +0100

    Model model created

---
+++ api/src/models/Model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const modelSchema = new mongoose.Schema({
+    мodel: {
+        type: String,
+        required: true,
+        validate: [
+            /^[a-zA-Z0-9 ]+$/,
+            "Brand should contain only english letters,digits and spaces",
+        ],
+    },
+    avgPrice: {
+        type: Number,
+        required: true,
+        validate: [/^[0-9]+$/, "Average price should contain only digits"],
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 100,
+    },
+    images: [
+        {
+            type: String,
+            required: true,
+            validate: [
+                /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/,
+                "Please enter a valid URL",
+            ],
+        },
+    ],
+});
+
+const Model = mongoose.model("Model", modelSchema);
+
+module.exports = Model;
