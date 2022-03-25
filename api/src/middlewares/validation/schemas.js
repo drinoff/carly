399bce8649badcfb2d6c6194c6103c1e9a2687a5
Drinoff <drinoff@gmail.com>
2022-03-26 00:51:44 +0100

    yup register schema redesigned

--- api/src/middlewares/validation/schemas.js
+++ api/src/middlewares/validation/schemas.js
@@ -1,5 +1,11 @@
 const yup = require("yup");
 
+yup.addMethod(yup.array, "unique", function (message, mapper = (a) => a) {
+    return this.test("unique", message, function (list) {
+        return list.length === new Set(list.map(mapper)).size;
+    });
+});
+
 const registerSchema = yup.object().shape({
     email: yup.string().email("Please input a valid email").required(),
     password: yup
