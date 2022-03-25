6301dfa7a0f4b9eb41c63cf5d1e5e987e884c480
Drinoff <drinoff@gmail.com>
2022-03-26 00:52:01 +0100

    bugs fixed

--- api/src/middlewares/validation/validate.js
+++ api/src/middlewares/validation/validate.js
@@ -1,13 +1,14 @@
 const validate = (schema) => async (req, res, next) => {
-    try {
-        console.log(req.body);
-        await schema.validate({
-            body: req.body,
+    return schema
+        .validate({
+            ...req.body,
+        })
+        .then(() => next())
+        .catch((err) => {
+            res.status(400).json({
+                error: err.message,
+            });
         });
-        return next();
-    } catch (err) {
-        return res.status(500).json({ type: err.name, message: err.message });
-    }
 };
 
 module.exports = validate;
