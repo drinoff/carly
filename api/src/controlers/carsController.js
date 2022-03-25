cf0714658f7a966209ed2ee9341aeb3db708d30b
Drinoff <drinoff@gmail.com>
2022-03-25 18:00:57 +0100

    console.log cleared

--- api/src/controlers/carsController.js
+++ api/src/controlers/carsController.js
@@ -3,7 +3,6 @@ const carServices = require("../services/carServices");
 
 const getAllCars = (req, res) => {
     carServices.getAllCars().then((cars) => {
-        console.log(cars);
         res.json(cars);
     });
 };
