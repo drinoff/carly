4aa459386b11dfeecd9c399b9e4080d940bc519e
Drinoff <drinoff@gmail.com>
2022-03-25 06:33:47 +0100

    user route added

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const carsController = require("./controlers/carsController");
+const userController = require("./controlers/userController");
 
 router.use("/cars", carsController);
+router.use("/user", userController);
 
 module.exports = router;
