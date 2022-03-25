04e9eb2d9027d6b5a826376fae9c871ed5041564
Drinoff <drinoff@gmail.com>
2022-03-25 06:34:27 +0100

    Register view added styled

---
+++ client/src/components/Register/Register.js
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+
+import { Box } from "@mui/material";
+import "./Register.css";
+
+const Register = () => {
+    const onRegisterFormSubmitHandler = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <Box
+            className="registerContainer"
+            sx={{ bgcolor: "#111827", height: "80%", width: "33%" }}
+        >
+            <h1 className="pleaseRegister">Please Register</h1>
+            <form
+                id="register-form"
+                action="POST"
+                className="register-form"
+                onSubmit={onRegisterFormSubmitHandler}
+            >
+                <div className="emailContainer">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="text"
+                        name="email"
+                        className="registerFormInputStyle"
+                    />
+                </div>
+
+                <div className="passwordContainer">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        className="registerFormInputStyle"
+                    />
+                </div>
+                <div className="rePassContainer">
+                    <label htmlFor="rePass">Repeat Password</label>
+                    <input
+                        id="rePass"
+                        type="password"
+                        name="rePass"
+                        className="registerFormInputStyle"
+                    />
+                </div>
+
+                <input
+                    className="registerButton"
+                    type="submit"
+                    value="Register"
+                />
+                <p className="alreadyGotAcc">
+                    Don't have an account?{" "}
+                    <Link to="/login" className="loginLinkInRegisterComp">
+                        <span>Login</span>
+                    </Link>
+                </p>
+            </form>
+        </Box>
+    );
+};
+
+export default Register;
