b2cc689ffa49e9a4bd6724849c70e30548364bda
Drinoff <drinoff@gmail.com>
2022-03-25 06:34:14 +0100

    user services initial commit

---
+++ client/src/services/userServices.js
@@ -0,0 +1,10 @@
+const register = (userData) => {}; // validate if passes match and other validation => send to backend for DB validation
+
+const login = (userData) => {}; //chechk if passwords match send to backEnd for further validation with DB entity
+
+const userServices = {
+    register,
+    login,
+};
+
+export default userServices;
