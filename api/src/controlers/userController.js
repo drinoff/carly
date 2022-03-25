2563003b729cee6c0f90b6ac2cf48062f967c3d5
Drinoff <drinoff@gmail.com>
2022-03-25 18:01:26 +0100

    register functinality added

--- api/src/controlers/userController.js
+++ api/src/controlers/userController.js
@@ -1,1 +1,20 @@
 const router = require("express").Router();
+
+const userServices = require("./../services/userServices");
+const validate = require("../middlewares/validation/validate");
+const schemas = require("../middlewares/validation/schemas");
+
+const register = (req, res) => {
+    return userServices
+        .register(req.body)
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+};
+
+router.post("/register", (validate(schemas.registerSchema), register));
+
+module.exports = router;
