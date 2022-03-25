f4af1ec9d4ee3cc9638b153e1d8f476389efa0a7
Drinoff <drinoff@gmail.com>
2022-03-25 18:02:45 +0100

    register services for user added

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -1,4 +1,14 @@
-const register = (userData) => {}; // validate if passes match and other validation => send to backend for DB validation
+import { BASE_URL } from "../constants";
+
+const register = (userData) => {
+    return fetch(BASE_URL + "/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+    });
+}; // validate if passes match and other validation => send to backend for DB validation
 
 const login = (userData) => {}; //chechk if passwords match send to backEnd for further validation with DB entity
 
