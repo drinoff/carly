a90f6fb78d2522c320113596eaadfb49c5f9e030
Drinoff <drinoff@gmail.com>
2022-03-27 13:04:39 +0200

    login functionality added to userServices

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -12,9 +12,18 @@ const register = (userData) => {
     });
 };
 
-// validate if passes match and other validation => send to backend for DB validation
-
-const login = (userData) => {}; //chechk if passwords match send to backEnd for further validation with DB entity
+const login = (userData) => {
+    return fetch(BASE_URL + "/user/login", {
+        method: "POST",
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+    }).then((response) => {
+        return response.json();
+    });
+};
 
 const userServices = {
     register,
