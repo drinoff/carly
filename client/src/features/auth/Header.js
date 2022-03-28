a982fad58419ba2b88f481e2ebbee44f72c2f0ce
Drinoff <drinoff@gmail.com>
2022-03-28 23:12:34 +0200

    Header moved to match redux structure

---
+++ client/src/features/auth/Header.js
@@ -0,0 +1,19 @@
+import NavBar from "../../components/NavBar/NavBar";
+import { useSelector } from "react-redux";
+import { userSelector, isAuthenticatedSelector } from "./authSlice";
+import "./Header.css";
+import User from "../../components/User/User";
+
+const Header = () => {
+    const user = useSelector(userSelector);
+    const isAuthenticated = useSelector(isAuthenticatedSelector);
+    console.log(user, isAuthenticated);
+    return (
+        <div className="header">
+            <User isAuthenticated={isAuthenticated} user={user} />
+            <img className="header-logo" src="images/logo.png" alt="" />
+            <NavBar isAuthenticated={isAuthenticated} />
+        </div>
+    );
+};
+export default Header;
