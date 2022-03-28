d0797dd9fba556d2a676cdce255642397bb04866
Drinoff <drinoff@gmail.com>
2022-03-28 23:10:26 +0200

    refactored App.js, removed unnecesary imports , moved imports to match redux structure

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,66 +1,41 @@
 import { Routes, Route } from "react-router-dom";
-import useLocalStorage from "../hooks/useLocalStorage";
 
-import Header from "../components/Header/Header";
+import Header from "../features/auth/Header";
 import Home from "../components/Home/Home";
 import Cars from "../features/car/Cars";
 import BrandDetails from "../components/Cars/BrandDetails/BrandDetails";
 import ModelDetails from "../components/Cars/BrandDetails/CarModelButton/ModelDetails/ModelDetails";
 import Blog from "../features/review/Review";
-import UserContext from "../contexts/UserContext";
 import Login from "../components/Login/Login";
 import Register from "../components/Register/Register";
+import UserPanel from "../components/UserPanel/UserPanel";
 
 import "./App.css";
-
-const initialValue = {
-    email: "",
-    accesToken: "",
-    _id: "",
-    isAuthenticated: false,
-};
+import Logout from "../components/Logout/Logout";
 
 function App() {
-    const [user, setUser] = useLocalStorage("user", initialValue);
-    const [message, setMessage] = useLocalStorage("message", "");
-    const [error, setError] = useLocalStorage("error", "");
-
-    const onLogin = (user) => {
-        console.log(user);
-        setUser(user);
-    };
-    const onLogout = () => {
-        setUser(null);
-    };
-
     const onError = (error) => {
         console.log(error);
     };
 
     return (
         <div className="App">
-            <UserContext.Provider value={{ user, onLogin, onLogout }}>
-                <Header />
+            <Header />
 
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="cars" element={<Cars />} />
-                    <Route path="cars/:brand" element={<BrandDetails />} />
-                    <Route
-                        path="cars/:brand/:model"
-                        element={<ModelDetails />}
-                    />
-                    <Route path="blog" element={<Blog />} />
-                    <Route
-                        path="register"
-                        element={<Register onError={onError} />}
-                    />
-                    <Route
-                        path="login"
-                        element={<Login onLogin={onLogin} onError={onError} />}
-                    />
-                </Routes>
-            </UserContext.Provider>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="cars" element={<Cars />} />
+                <Route path="cars/:brand" element={<BrandDetails />} />
+                <Route path="cars/:brand/:model" element={<ModelDetails />} />
+                <Route path="blog" element={<Blog />} />
+                <Route
+                    path="register"
+                    element={<Register onError={onError} />}
+                />
+                <Route path="login" element={<Login onError={onError} />} />
+                <Route path="logout" element={<Logout />} />
+                <Route path="user/:name" element={<UserPanel />} />
+            </Routes>
         </div>
     );
 }
