55d1a6acf38fdc6ccaaa1e2758a56fa4da713a81
Drinoff <drinoff@gmail.com>
2022-03-28 16:54:15 +0200

    reviews route added

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const carsController = require("./controlers/carsController");
 const userController = require("./controlers/userController");
+const reviewsController = require("./controlers/reviewsController");
 
 router.use("/cars", carsController);
 router.use("/user", userController);
+router.use("/reviews", reviewsController);
 
 module.exports = router;
