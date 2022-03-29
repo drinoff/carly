79d169447bba801d67f1960919bc24d44126a09a
Drinoff <drinoff@gmail.com>
2022-03-29 23:44:09 +0200

    classifiedReducer added to combineReducers

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import carReducer from "../features/car/carSlice";
 import reviewReducer from "../features/review/reviewSlice";
 import authReducer from "../features/auth/authSlice";
+import classifiedReducer from "../features/classifieds/classifiedSlice";
 
 export const store = configureStore({
     reducer: {
         cars: carReducer,
         reviews: reviewReducer,
         auth: authReducer,
+        classifieds: classifiedReducer,
     },
 });
