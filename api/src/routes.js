b8beb842b4a666d6adfa08a82c7460f955c3cf0f
Drinoff <drinoff@gmail.com>
2022-03-29 23:42:30 +0200

    routes updated

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const carsController = require("./controlers/carsController");
 const userController = require("./controlers/userController");
 const reviewsController = require("./controlers/reviewsController");
+const classifiedsController = require("./controlers/classifiedsController");
 
 router.use("/cars", carsController);
 router.use("/user", userController);
 router.use("/reviews", reviewsController);
+router.use("/classifieds", classifiedsController);
 
 module.exports = router;
