88a6350ec5bce7a89db399b6974467d5c3469d80
Drinoff <drinoff@gmail.com>
2022-04-01 00:15:49 +0200

    AddModelButton created styled

---
+++ client/src/components/Cars/BrandDetails/CarModelButton/AddModelButton/AddModelButton.js
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
+import "./AddModelButton.css";
+
+const AddModelButton = ({ car }) => {
+    const carBrand = car.brand;
+
+    const navigate = useNavigate();
+
+    const onAddModelClickHandler = () => {
+        navigate(`/cars/${carBrand}/addModel`, { state: { car } });
+    };
+    return (
+        <button
+            className="AddModelButton-modelButton"
+            onClick={onAddModelClickHandler}
+        >
+            Add Model
+        </button>
+    );
+};
+
+export default AddModelButton;
