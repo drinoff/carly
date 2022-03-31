91f819c7a8bbbf3f71859618ece662c71950378d
Drinoff <drinoff@gmail.com>
2022-04-01 00:12:22 +0200

    addModel yup schema created

--- api/src/middlewares/validation/schemas.js
+++ api/src/middlewares/validation/schemas.js
@@ -26,9 +26,41 @@ const loginSchema = yup.object().shape({
     password: yup.string().required("Password field cannot be empty"),
 });
 
+const addCarSchema = yup.object().shape({
+    brand: yup.string().min(2).required("Brand field cannot be empty"),
+    logo: yup
+        .string()
+        .matches(
+            /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
+            "Please enter a valid logo URL"
+        )
+        .required("Logo field cannot be empty"),
+    brandHistory: yup
+        .string()
+        .min(100)
+        .required("Brand History field cannot be empty"),
+});
+
+const addModelSchema = yup.object().shape({
+    model: yup.string().min(2).required("Model Name field cannot be empty"),
+    avgPrice: yup.number().required("Average Price field cannot be empty"),
+    description: yup
+        .string()
+        .min(100)
+        .required("Description field cannot be empty"),
+    pictures: yup
+        .string()
+        .matches(
+            /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
+            "Please enter a valid URL"
+        ),
+});
+
 const yupSchemas = {
     registerSchema,
     loginSchema,
+    addCarSchema,
+    addModelSchema,
 };
 
 module.exports = yupSchemas;
