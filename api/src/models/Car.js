788a911704e29313c20cf1998d0fc2a192f66356
Drinoff <drinoff@gmail.com>
2022-04-01 00:12:41 +0200

    Car model bug cleared

--- api/src/models/Car.js
+++ api/src/models/Car.js
@@ -25,7 +25,7 @@ const carSchema = new mongoose.Schema({
         type: String,
         required: true,
         validate: [
-            /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/,
+            /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
             "Please enter a valid URL",
         ],
     },
