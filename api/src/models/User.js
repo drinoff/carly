fcd18036ebeb380db9923f4ea04737d5c92d56a7
Drinoff <drinoff@gmail.com>
2022-04-01 00:13:10 +0200

    User model role added

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -10,6 +10,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    role: {
+        type: String,
+        enum: ["user", "reviewer", "admin"],
+        default: "user",
+    },
     classifieds: [
         {
             type: mongoose.Schema.Types.ObjectId,
