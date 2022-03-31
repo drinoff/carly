b850741e338b948e4ab5738abea30650c3a8f721
Drinoff <drinoff@gmail.com>
2022-04-01 00:16:29 +0200

    CarList updated to show button on auth

--- client/src/components/Cars/CarList/CarList.js
+++ client/src/components/Cars/CarList/CarList.js
@@ -1,7 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import Car from "./Car/Car";
+import AddCarButton from "../AddCar/AddCarButton";
 import "./CarList.css";
+import {
+    isAuthenticatedSelector,
+    userSelector,
+} from "../../../features/auth/authSlice";
 
 const CarList = ({ cars, carClickHandler }) => {
+    const user = useSelector(userSelector);
+    const isAuthenticated = useSelector(isAuthenticatedSelector);
+
     return (
         <div className="CarList-container">
             {cars.map((car) => (
@@ -11,6 +21,11 @@ const CarList = ({ cars, carClickHandler }) => {
                     carClickHandler={carClickHandler}
                 />
             ))}
+            {isAuthenticated ? (
+                user.role === "admin" ? (
+                    <AddCarButton />
+                ) : null
+            ) : null}
         </div>
     );
 };
