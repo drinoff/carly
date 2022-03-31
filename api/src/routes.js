a1678fca5a8c458e914e21e7897635572a8ea5f6
Drinoff <drinoff@gmail.com>
2022-04-01 00:13:28 +0200

    routes updated to have model route

--- api/src/routes.js
+++ api/src/routes.js
@@ -3,8 +3,10 @@ const carsController = require("./controlers/carsController");
 const userController = require("./controlers/userController");
 const reviewsController = require("./controlers/reviewsController");
 const classifiedsController = require("./controlers/classifiedsController");
+const modelController = require("./controlers/modelController");
 
 router.use("/cars", carsController);
+router.use("/models", modelController);
 router.use("/user", userController);
 router.use("/reviews", reviewsController);
 router.use("/classifieds", classifiedsController);
