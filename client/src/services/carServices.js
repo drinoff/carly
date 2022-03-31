578b03a0ab1c55fe849ebd30da75e8a3dce6fac9
Drinoff <drinoff@gmail.com>
2022-04-01 00:18:13 +0200

    carServices created

--- client/src/services/carServices.js
+++ client/src/services/carServices.js
@@ -8,8 +8,38 @@ const getAllCars = async () => {
     );
 };
 
+const addCar = async (car) => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    return fetch(`${BASE_URL}/cars`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-authorization": user.accessToken,
+        },
+        body: JSON.stringify(car),
+    }).then((res) => res.json());
+};
+
+const addModel = (model) => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    return fetch(`${BASE_URL}/models`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-authorization": user.accessToken,
+        },
+        body: JSON.stringify(model),
+    })
+        .then((res) => res.json())
+        .catch((err) => {
+            return err.message;
+        });
+};
+
 const carServices = {
     getAllCars,
+    addCar,
+    addModel,
 };
 
 export default carServices;
