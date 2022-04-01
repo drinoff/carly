934d98e3ca5246d55186c93bb98b7bddcb3de75b
Drinoff <drinoff@gmail.com>
2022-04-02 00:01:31 +0200

    Update reviewsController.js

--- api/src/controlers/reviewsController.js
+++ api/src/controlers/reviewsController.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const reviewServices = require("../services/reviewServices");
 
 router.get("/", (req, res) => {
-    reviewServices
-        .getAllReviews()
-        .then((reviews) => {
-            res.send(reviews);
-        })
-        .catch((err) => {
-            res.send(err);
-        });
+	reviewServices
+		.getAllReviews()
+		.then((reviews) => {
+			res.send(reviews);
+		})
+		.catch((err) => {
+			res.send(err);
+		});
 });
 
 module.exports = router;
