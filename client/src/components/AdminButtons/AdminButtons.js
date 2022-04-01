46e355bfe46830e0156947da64f0abeaab18eb50
Drinoff <drinoff@gmail.com>
2022-04-02 00:01:59 +0200

    AdminButtons component created

---
+++ client/src/components/AdminButtons/AdminButtons.js
@@ -0,0 +1,54 @@
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+    isAuthenticatedSelector,
+    userSelector,
+} from "../../features/auth/authSlice";
+import carServices from "../../services/carServices";
+import "./AdminButtons.css";
+
+const AdminButtons = ({ carId, carBrand, carModels }) => {
+    const navigate = useNavigate();
+    const user = useSelector(userSelector);
+    const isAuthenticated = useSelector(isAuthenticatedSelector);
+
+    const onCarEditClickHandler = () => {
+        navigate(`/cars/${carBrand}/edit`, { state: { carId, carModels } });
+    };
+
+    const onCarDeleteCLickHandler = () => {
+        carServices
+            .deleteCar(carId)
+            .then(() => {
+                navigate("/cars");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    return (
+        <>
+            {isAuthenticated ? (
+                user.role === "admin" ? (
+                    <div className="adminButton">
+                        <button
+                            className="BrandDetails-Edit"
+                            onClick={onCarEditClickHandler}
+                        >
+                            Edit
+                        </button>
+                        <button
+                            className="BrandDetails-Delete"
+                            onClick={onCarDeleteCLickHandler}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                ) : null
+            ) : null}
+        </>
+    );
+};
+
+export default AdminButtons;
