c49cb56e189d2e3f810cf629fc5a4354291c8655
Drinoff <drinoff@gmail.com>
2022-04-02 00:04:08 +0200

    more services added

--- client/src/services/carServices.js
+++ client/src/services/carServices.js
@@ -1,45 +1,124 @@
 import { BASE_URL } from "../constants";
 
+const user = JSON.parse(localStorage.getItem("user"));
+const accessToken = user.accessToken;
+
 const getAllCars = async () => {
-    return fetch(`${BASE_URL}/cars`).then((res) =>
-        res.json().catch((err) => {
-            return err.message;
-        })
-    );
+	return fetch(`${BASE_URL}/cars`).then((res) =>
+		res.json().catch((err) => {
+			return err.message;
+		})
+	);
 };
 
 const addCar = async (car) => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    return fetch(`${BASE_URL}/cars`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "x-authorization": user.accessToken,
-        },
-        body: JSON.stringify(car),
-    }).then((res) => res.json());
+	return fetch(`${BASE_URL}/cars`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"x-authorization": accessToken,
+		},
+		body: JSON.stringify(car),
+	}).then((res) => res.json());
+};
+
+const getCarById = (carId) => {
+	return fetch(`${BASE_URL}/cars/${carId}`).then((res) =>
+		res.json().catch((err) => {
+			return err.message;
+		})
+	);
+};
+
+const updateCar = (carId, carData) => {
+	return fetch(`${BASE_URL}/cars/${carId}`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+			"x-authorization": accessToken,
+		},
+		body: JSON.stringify(carData),
+	})
+		.then((res) => res.json())
+		.catch((err) => err.message);
+};
+
+const deleteCar = (carId) => {
+	return fetch(`${BASE_URL}/cars/${carId}`, {
+		method: "DELETE",
+		headers: {
+			"Content-Type": "application/json",
+			"x-authorization": accessToken,
+		},
+	})
+		.then((res) => res.json())
+		.catch((err) => {
+			return err.message;
+		});
 };
 
 const addModel = (model) => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    return fetch(`${BASE_URL}/models`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "x-authorization": user.accessToken,
-        },
-        body: JSON.stringify(model),
-    })
-        .then((res) => res.json())
-        .catch((err) => {
-            return err.message;
-        });
+	return fetch(`${BASE_URL}/models`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"x-authorization": accessToken,
+		},
+		body: JSON.stringify(model),
+	})
+		.then((res) => res.json())
+		.catch((err) => {
+			return err.message;
+		});
+};
+
+const getModelById = (modelId) => {
+	return fetch(`${BASE_URL}/models/${modelId}`).then((res) =>
+		res.json().catch((err) => {
+			return err.message;
+		})
+	);
+};
+
+const updateModel = (modelId, modelData) => {
+	return fetch(`${BASE_URL}/models/${modelId}`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+			"x-authorization": accessToken,
+		},
+		body: JSON.stringify(modelData),
+	})
+		.then((res) => res.json())
+		.catch((err) => {
+			return err.message;
+		});
+};
+
+const deleteModel = (modelId) => {
+	return fetch(`${BASE_URL}/models/${modelId}`, {
+		method: "DELETE",
+		headers: {
+			"Content-Type": "application/json",
+			"x-authorization": accessToken,
+		},
+	})
+		.then((res) => res.json())
+		.catch((err) => {
+			return err.message;
+		});
 };
 
 const carServices = {
-    getAllCars,
-    addCar,
-    addModel,
+	getAllCars,
+	addCar,
+	deleteCar,
+	getCarById,
+	updateCar,
+	addModel,
+	getModelById,
+	updateModel,
+	deleteModel,
 };
 
 export default carServices;
