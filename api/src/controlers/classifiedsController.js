49f192a4339fc04a74bbc9e511d71d0a531d365e
Drinoff <drinoff@gmail.com>
2022-04-02 00:01:12 +0200

    Update classifiedsController.js

--- api/src/controlers/classifiedsController.js
+++ api/src/controlers/classifiedsController.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const classifiedServices = require("../services/classifiedServices");
 
 const getAllClassifieds = (req, res) => {
-    classifiedServices
-        .getAllClassifieds()
-        .then((classifieds) => {
-            res.json(classifieds);
-        })
-        .catch((err) => {
-            res.json(err);
-        });
+	classifiedServices
+		.getAllClassifieds()
+		.then((classifieds) => {
+			res.json(classifieds);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
 };
 
 router.get("/", getAllClassifieds);
