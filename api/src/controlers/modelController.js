b5b1e8b08e88a330bebec0c57e37b976e3538864
Drinoff <drinoff@gmail.com>
2022-04-02 00:01:26 +0200

    routes with guards added

--- api/src/controlers/modelController.js
+++ api/src/controlers/modelController.js
@@ -5,26 +5,62 @@ const schemas = require("../middlewares/validation/schemas");
 const carServices = require("../services/carServices");
 
 const addModel = (req, res) => {
-    const { brandId } = req.body;
-    delete req.body.brandId;
-    const model = req.body;
+	const { brandId } = req.body;
+	delete req.body.brandId;
+	const model = req.body;
 
-    return carServices
-        .addModel(model, brandId)
-        .then((res) => {
-            return res;
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+	return carServices
+		.addModel(model, brandId)
+		.then((res) => {
+			res.status(201).json(res);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
-router.post(
-    "/",
-    isAuthorized(),
-    isAdmin(),
-    validate(schemas.addModelSchema),
-    addModel
-);
+const getModelById = (req, res) => {
+	const { modelId } = req.params;
+	return carServices
+		.getModelById(modelId)
+		.then((model) => {
+			res.json(model);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+const updateModel = (req, res) => {
+	const { modelId } = req.params;
+	const model = req.body;
+	return carServices
+		.updateModel(modelId, model)
+		.then((model) => {
+			res.json(model);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+const deleteModel = (req, res) => {
+	const { modelId } = req.params;
+
+	return carServices
+		.deleteModel(modelId)
+		.then((model) => {
+			res.json(model);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+router.post("/", isAuthorized(), isAdmin(), validate(schemas.addModelSchema), addModel);
+
+router.get("/:modelId", getModelById);
+router.put("/:modelId", isAuthorized(), isAdmin(), validate(schemas.addModelSchema), updateModel);
+router.delete("/:modelId", isAuthorized(), isAdmin(), deleteModel);
 
 module.exports = router;
