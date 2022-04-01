b71d13a4da753f7414a4b54f91ca6c90a85ea3ff
Drinoff <drinoff@gmail.com>
2022-04-02 00:02:37 +0200

    logic changed to fetchById

--- client/src/components/Cars/BrandDetails/BrandDetails.js
+++ client/src/components/Cars/BrandDetails/BrandDetails.js
@@ -1,15 +1,30 @@
 import { useLocation, useNavigate } from "react-router-dom";
-
+import { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import CarModelButton from "./CarModelButton/CarModelButton";
 import AddModelButton from "../BrandDetails/CarModelButton/AddModelButton/AddModelButton";
+import AdminButtons from "../../AdminButtons/AdminButtons";
 
 import "./BrandDetails.css";
+import carServices from "../../../services/carServices";
 
 const BrandDetails = () => {
+    const [car, setCar] = useState();
     const navigate = useNavigate();
     const location = useLocation();
-    const car = location.state.car;
+    const carId = location.state.carId;
+    const carModels = location.state.carModels;
+
+    useEffect(() => {
+        carServices
+            .getCarById(carId)
+            .then((car) => {
+                setCar(car);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [carId]);
 
     const onCarModelClickHandler = (e) => {
         const currentModel = car.models.find(
@@ -33,11 +48,11 @@ const BrandDetails = () => {
                 width: "70%",
             }}
         >
-            <h1>{car.brand}</h1>
-            <img className="bounce-in-top" src={car.logo} alt="Car-logo" />
+            <h1>{car?.brand}</h1>
+            <img className="bounce-in-top" src={car?.logo} alt="Car-logo" />
             <p className="CarDetails-popularModels">Popular Models</p>
             <div className="BrandDetails-modelButton">
-                {car.models.map((model) => (
+                {car?.models.map((model) => (
                     <CarModelButton
                         key={model.model}
                         carModel={model.model}
@@ -47,10 +62,15 @@ const BrandDetails = () => {
                 <AddModelButton car={car} />
             </div>
             <h2>Brief History</h2>
-            <p className="BrandDetails-description">{car.brandHistory}</p>
+            <p className="BrandDetails-description">{car?.brandHistory}</p>
             <button className="backBtn" onClick={onBackButtonClickHandler}>
                 Back
             </button>
+            <AdminButtons
+                carId={car?._id}
+                carBrand={car?.brand}
+                carModels={carModels}
+            />
         </Box>
     );
 };
