0b30ac895c8ec53d8bcb0064997c0401b88831c1
Drinoff <drinoff@gmail.com>
2022-04-02 00:01:46 +0200

    routes added

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "../features/auth/Header";
 import Home from "../components/Home/Home";
 import Cars from "../features/car/Cars";
+import EditCar from "../components/Cars/EditCar/EditCar";
 import BrandDetails from "../components/Cars/BrandDetails/BrandDetails";
 import ModelDetails from "../components/Cars/BrandDetails/CarModelButton/ModelDetails/ModelDetails";
 import Blog from "../features/review/Review";
@@ -12,64 +13,75 @@ import Classifieds from "../features/classifieds/Classifieds";
 import DetailedClassified from "../components/Classified/DetailedClassified/DetailedClassified";
 import NotFound from "../components/NotFound/NotFound";
 import RouteGuard from "../components/RouteGuard/RouteGuard";
-
-import UserPanel from "../components/UserPanel/UserPanel";
-
-import "./App.css";
 import Logout from "../components/Logout/Logout";
 import AddCar from "../components/Cars/AddCar/AddCar";
 import AddModel from "../components/Cars/BrandDetails/AddModel/AddModel";
+import EditModel from "../components/Cars/EditModel/EditModel";
+import UserPanel from "../components/UserPanel/UserPanel";
+
+import "./App.css";
 
 function App() {
-    const onError = (error) => {
-        console.log(error);
-    };
+	const onError = (error) => {
+		console.log(error);
+	};
 
-    return (
-        <div className="App">
-            <Header />
+	return (
+		<div className="App">
+			<Header />
 
-            <Routes>
-                <Route path="*" element={<NotFound />} />
-                <Route path="/" element={<Home />} />
-                <Route path="cars" element={<Cars />} />
-                <Route path="cars/:brand" element={<BrandDetails />} />
-                <Route path="cars/:brand/:model" element={<ModelDetails />} />
+			<Routes>
+				<Route path="*" element={<NotFound />} />
+				<Route path="/" element={<Home />} />
+				<Route path="cars" element={<Cars />} />
+				<Route path="cars/:brand" element={<BrandDetails />} />
+				<Route path="cars/:brand/:model" element={<ModelDetails />} />
 
-                <Route
-                    path="cars/:brand/addModel"
-                    element={
-                        <RouteGuard>
-                            <AddModel />
-                        </RouteGuard>
-                    }
-                />
+				<Route
+					path="cars/:brand/edit"
+					element={
+						<RouteGuard>
+							<EditCar />
+						</RouteGuard>
+					}
+				/>
+				<Route
+					path="cars/:brand/:model/edit"
+					element={
+						<RouteGuard>
+							<EditModel />
+						</RouteGuard>
+					}
+				/>
 
-                <Route
-                    path="cars/add"
-                    element={
-                        <RouteGuard>
-                            <AddCar />
-                        </RouteGuard>
-                    }
-                />
+				<Route
+					path="cars/:brand/addModel"
+					element={
+						<RouteGuard>
+							<AddModel />
+						</RouteGuard>
+					}
+				/>
 
-                <Route path="blog" element={<Blog />} />
-                <Route
-                    path="register"
-                    element={<Register onError={onError} />}
-                />
-                <Route path="login" element={<Login onError={onError} />} />
-                <Route path="logout" element={<Logout />} />
-                <Route path="classifieds" element={<Classifieds />} />
-                <Route
-                    path="classifieds/:id"
-                    element={<DetailedClassified />}
-                />
-                <Route path="user/:name" element={<UserPanel />} />
-            </Routes>
-        </div>
-    );
+				<Route
+					path="cars/add"
+					element={
+						<RouteGuard>
+							<AddCar />
+						</RouteGuard>
+					}
+				/>
+
+				<Route path="blog" element={<Blog />} />
+				<Route path="register" element={<Register onError={onError} />} />
+				<Route path="login" element={<Login onError={onError} />} />
+				<Route path="logout" element={<Logout />} />
+				<Route path="classifieds" element={<Classifieds />} />
+				<Route path="classifieds/:id" element={<DetailedClassified />} />
+				<Route path="user/:name" element={<UserPanel />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
