b79e6f0ebd14b137eb6d5dcbe15e97101817a787
Drinoff <drinoff@gmail.com>
2022-04-02 15:41:43 +0200

    review services implemented

--- client/src/services/reviewServices.js
+++ client/src/services/reviewServices.js
@@ -1,15 +1,32 @@
 import { BASE_URL } from "../constants";
 
+const user = JSON.parse(localStorage.getItem("user"));
+const accessToken = user.accessToken;
+
 const getAllReviews = async () => {
-    return fetch(`${BASE_URL}/reviews`)
-        .then((res) => res.json())
-        .catch((err) => {
-            return err.message;
-        });
+	return fetch(`${BASE_URL}/reviews`)
+		.then((res) => res.json())
+		.catch((err) => {
+			return err.message;
+		});
+};
+
+const addReview = (review) => {
+	return fetch(`${BASE_URL}/reviews`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"x-authorization": accessToken,
+		},
+		body: JSON.stringify(review),
+	})
+		.then((res) => res.json())
+		.catch((err) => err.message);
 };
 
 const blogServices = {
-    getAllReviews,
+	getAllReviews,
+	addReview,
 };
 
 export default blogServices;
