3b128e82a6c4bfed86b8ae2431a4daa4d2e8c56b
Drinoff <drinoff@gmail.com>
2022-04-02 15:42:51 +0200

    navBar styling updated

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -3,44 +3,45 @@ import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = ({ isAuthenticated }) => {
-    let activeStyle = {
-        backgroundColor: "#242A38",
-        border: "1px solid #7582EB",
-        borderRadius: "5px",
-    };
-    const ACTIVE = ({ isActive }) => (isActive ? activeStyle : undefined);
-    return (
-        <ul className="navigation">
-            <NavLink to="/" style={ACTIVE} className="li">
-                Home
-            </NavLink>
-            <NavLink to="cars" style={ACTIVE} className="li">
-                Cars
-            </NavLink>
-            <NavLink to="blog" style={ACTIVE} className="li">
-                Blog
-            </NavLink>
-            <NavLink to="classifieds" style={ACTIVE} className="li">
-                Clasifieds
-            </NavLink>
-            <NavLink to="about" style={ACTIVE} className="li">
-                About
-            </NavLink>
-            {isAuthenticated ? (
-                <NavLink to="logout" className="logout li">
-                    Logout
-                </NavLink>
-            ) : (
-                <>
-                    <NavLink to="login" className="login li">
-                        Login
-                    </NavLink>
-                    <NavLink to="register" className="register li">
-                        Register
-                    </NavLink>{" "}
-                </>
-            )}
-        </ul>
-    );
+	let activeStyle = {
+		backgroundColor: "#242A38",
+		border: "1px solid #7582EB",
+		borderRadius: "5px",
+		color: "#75FAC8",
+	};
+	const ACTIVE = ({ isActive }) => (isActive ? activeStyle : undefined);
+	return (
+		<ul className="navigation">
+			<NavLink to="/" style={ACTIVE} className="li">
+				Home
+			</NavLink>
+			<NavLink to="cars" style={ACTIVE} className="li">
+				Cars
+			</NavLink>
+			<NavLink to="blog" style={ACTIVE} className="li">
+				Blog
+			</NavLink>
+			<NavLink to="classifieds" style={ACTIVE} className="li">
+				Clasifieds
+			</NavLink>
+			<NavLink to="about" style={ACTIVE} className="li">
+				About
+			</NavLink>
+			{isAuthenticated ? (
+				<NavLink to="logout" className="logout li">
+					Logout
+				</NavLink>
+			) : (
+				<>
+					<NavLink to="login" className="login li">
+						Login
+					</NavLink>
+					<NavLink to="register" className="register li">
+						Register
+					</NavLink>{" "}
+				</>
+			)}
+		</ul>
+	);
 };
 export default NavBar;
