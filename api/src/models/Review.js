e8d08619c9de0cee52a58099e80b7a8a46d56cb1
Drinoff <drinoff@gmail.com>
2022-04-02 15:41:24 +0200

    bugs cleared

--- api/src/models/Review.js
+++ api/src/models/Review.js
@@ -1,42 +1,45 @@
 const mongoose = require("mongoose");
 
 const reviewSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    brand: {
-        type: String,
-        required: true,
-    },
-    model: {
-        type: String,
-        required: true,
-    },
-    desccription: {
-        type: String,
-        required: true,
-    },
-    embed: {
-        type: String,
-        required: true,
-    },
-    highs: {
-        type: String,
-        required: true,
-    },
-    lows: {
-        type: String,
-        required: true,
-    },
-    verdict: {
-        type: String,
-        required: true,
-    },
-    ownerId: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-    },
+	title: {
+		type: String,
+		required: true,
+	},
+	brand: {
+		type: String,
+		required: true,
+	},
+	model: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	embed: {
+		type: String,
+		required: true,
+	},
+	highs: {
+		type: String,
+		required: true,
+	},
+	lows: {
+		type: String,
+		required: true,
+	},
+	verdict: {
+		type: String,
+		required: true,
+	},
+	ownerId: {
+		type: mongoose.Types.ObjectId,
+		ref: "User",
+	},
+	regDate: {
+		type: Date,
+	},
 });
 
 const Review = mongoose.model("Review", reviewSchema);
