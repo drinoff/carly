028f8a39ea373e66d0ad7d5f2aeecd83c1714ed8
Drinoff <drinoff@gmail.com>
2022-04-02 15:42:31 +0200

    RevieAdminButtons implemented

---
+++ client/src/components/ReviewArticle/ReviewAdminButtons/ReviewAdminButtons.js
@@ -0,0 +1,12 @@
+import "./ReviewAdminButtons.css";
+
+const ReviewAdminButtons = () => {
+	return (
+		<div className="ReviewAdminButtons">
+			<button className="ReviewAdminButtons-edit">Edit</button>
+			<button className="ReviewAdminButtons-delete">Delete</button>
+		</div>
+	);
+};
+
+export default ReviewAdminButtons;
