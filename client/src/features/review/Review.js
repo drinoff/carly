2c9fa89f96752165f3a11b3e25cdec7ee376446c
Drinoff <drinoff@gmail.com>
2022-04-02 15:42:02 +0200

    Review component created

--- client/src/features/review/Review.js
+++ client/src/features/review/Review.js
@@ -1,33 +1,49 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { fetchReviews, selectAllReviews } from "./reviewSlice";
+import { userSelector, isAuthenticatedSelector } from "../auth/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 import { Box } from "@mui/material";
-import ReviewArticle from "../../components/RevieArticle/ReviewArticle";
+import ReviewArticle from "../../components/ReviewArticle/ReviewArticle";
 
 import "./Review.css";
 
 const Blog = () => {
-    const dispatch = useDispatch();
-    const reviews = useSelector(selectAllReviews);
+	const dispatch = useDispatch();
+	const reviews = useSelector(selectAllReviews);
+	const user = useSelector(userSelector);
+	const isAuthenticated = useSelector(isAuthenticatedSelector);
+	const navigate = useNavigate();
 
-    useEffect(() => {
-        dispatch(fetchReviews());
-    }, [dispatch]);
-    return (
-        <Box
-            className="Blog-blogContainer"
-            sx={{
-                bgcolor: "#111827",
-                height: "auto",
-                width: "70%",
-            }}
-        >
-            {reviews.map((review) => (
-                <ReviewArticle key={review.title} review={review} />
-            ))}
-        </Box>
-    );
+	useEffect(() => {
+		dispatch(fetchReviews());
+	}, [dispatch]);
+
+	const onAddArticleClickHandler = () => {
+		navigate(`/blog/add`);
+	};
+	return (
+		<Box
+			className="Blog-blogContainer"
+			sx={{
+				bgcolor: "#111827",
+				height: "auto",
+				width: "70%",
+			}}
+		>
+			{reviews.map((review) => (
+				<ReviewArticle key={review.title} review={review} />
+			))}
+			{isAuthenticated ? (
+				user.role === "admin" || user.role === "reviewer" ? (
+					<button className="addArticleButton" onClick={onAddArticleClickHandler}>
+						Add Article
+					</button>
+				) : null
+			) : null}
+		</Box>
+	);
 };
 
 export default Blog;
