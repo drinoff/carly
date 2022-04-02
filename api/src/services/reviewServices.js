0734d72675ba47d578003b0cf8cfc9d17f8a8dd0
Drinoff <drinoff@gmail.com>
2022-04-02 15:43:19 +0200

    review services created

--- api/src/services/reviewServices.js
+++ api/src/services/reviewServices.js
@@ -1,21 +1,33 @@
 const Review = require("../models/Review");
 
 const getAllReviews = () => {
-    return Review.find({})
-        .populate("ownerId", "email")
-        .lean()
-        .then((reviews) => {
-            console.log(reviews);
-            return reviews;
-        })
-        .catch((err) => {
-            console.log(err);
-            throw err;
-        });
+	return Review.find({})
+		.populate("ownerId", "email")
+		.lean()
+		.then((reviews) => {
+			console.log(reviews);
+			return reviews;
+		})
+		.catch((err) => {
+			console.log(err);
+			throw err;
+		});
+};
+
+const addReview = (review) => {
+	return Review.create(review)
+		.then((review) => {
+			return review;
+		})
+		.catch((err) => {
+			console.log(err);
+			throw err;
+		});
 };
 
 const reviewServices = {
-    getAllReviews,
+	getAllReviews,
+	addReview,
 };
 
 module.exports = reviewServices;
