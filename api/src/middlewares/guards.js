bf2f8463cf81d26dfc1016687d70a5d94d26a892
Drinoff <drinoff@gmail.com>
2022-04-02 15:40:51 +0200

    isReviewer guard implemented

--- api/src/middlewares/guards.js
+++ api/src/middlewares/guards.js
@@ -2,39 +2,56 @@ const { verifySession } = require("../services/userServices");
 const User = require("../models/User");
 
 function isAuthorized() {
-    return (req, res, next) => {
-        const token = req.headers["x-authorization"];
+	return (req, res, next) => {
+		const token = req.headers["x-authorization"];
 
-        try {
-            if (token) {
-                const userData = verifySession(token);
-                req.user = userData;
-            }
-            next();
-        } catch (err) {
-            res.status(498).json({
-                message: "Invalid access token. Please sign in",
-            });
-        }
-    };
+		try {
+			if (token) {
+				const userData = verifySession(token);
+				req.user = userData;
+			}
+			next();
+		} catch (err) {
+			res.status(498).json({
+				message: "Invalid access token. Please sign in",
+			});
+		}
+	};
 }
 
 function isAdmin() {
-    return async (req, res, next) => {
-        const userEmail = req.user.email;
-        const existing = await User.findOne({ email: userEmail });
+	return async (req, res, next) => {
+		const userEmail = req.user.email;
+		const existing = await User.findOne({ email: userEmail });
 
-        if (existing.role !== "admin") {
-            res.status(403).json({
-                message: "You are not authorized to perform this action",
-            });
-        } else {
-            next();
-        }
-    };
+		if (existing.role !== "admin") {
+			res.status(403).json({
+				message: "You are not authorized to perform this action",
+			});
+		} else {
+			next();
+		}
+	};
+}
+
+function isReviewer() {
+	return async (req, res, next) => {
+		const userEmail = req.user.email;
+		console.log(userEmail);
+		const existing = await User.findOne({ email: userEmail });
+
+		if (existing.role === "reviewer" || existing.role === "admin") {
+			next();
+		} else {
+			res.status(403).json({
+				message: "You are not authorized to perform this action",
+			});
+		}
+	};
 }
 
 module.exports = {
-    isAuthorized,
-    isAdmin,
+	isAuthorized,
+	isAdmin,
+	isReviewer,
 };
