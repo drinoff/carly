ea725f942d6cffe78789ad853aa7b8d14f18e411
Drinoff <drinoff@gmail.com>
2022-04-04 12:36:47 +0200

    cleared console logs

--- api/src/services/classifiedServices.js
+++ api/src/services/classifiedServices.js
@@ -1,21 +1,20 @@
 const Classified = require("../models/Classified");
 
 const getAllClassifieds = () => {
-    return Classified.find({})
-        .populate("ownerId", "email")
-        .lean()
-        .then((classifieds) => {
-            console.log(classifieds);
-            return classifieds;
-        })
-        .catch((err) => {
-            console.log(err);
-            throw err;
-        });
+	return Classified.find({})
+		.populate("ownerId", "email")
+		.lean()
+		.then((classifieds) => {
+			return classifieds;
+		})
+		.catch((err) => {
+			console.log(err);
+			throw err;
+		});
 };
 
 const classifiedServices = {
-    getAllClassifieds,
+	getAllClassifieds,
 };
 
 module.exports = classifiedServices;
