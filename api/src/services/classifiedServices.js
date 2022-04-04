d53317b4004b2a290e966a5a38ef25f97227e553
Drinoff <drinoff@gmail.com>
2022-04-04 23:43:29 +0200

    services added

--- api/src/services/classifiedServices.js
+++ api/src/services/classifiedServices.js
@@ -13,8 +13,41 @@ const getAllClassifieds = () => {
 		});
 };
 
+const addClassified = (classified) => {
+	return Classified.create(classified)
+		.then((classified) => {
+			return classified;
+		})
+		.catch((err) => {
+			throw err;
+		});
+};
+
+const updateClassified = (classifiedId, classified) => {
+	return Classified.findByIdAndUpdate(classifiedId, classified, { new: true })
+		.then((classified) => {
+			return classified;
+		})
+		.catch((err) => {
+			throw err;
+		});
+};
+
+const deleteClassified = (id) => {
+	return Classified.findByIdAndDelete(id)
+		.then((classified) => {
+			return classified;
+		})
+		.catch((err) => {
+			throw err;
+		});
+};
+
 const classifiedServices = {
 	getAllClassifieds,
+	addClassified,
+	deleteClassified,
+	updateClassified,
 };
 
 module.exports = classifiedServices;
