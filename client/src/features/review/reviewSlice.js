ee8e619980abb92928ee0e2d67abeaa980c021a9
Drinoff <drinoff@gmail.com>
2022-04-04 12:41:31 +0200

    formating

--- client/src/features/review/reviewSlice.js
+++ client/src/features/review/reviewSlice.js
@@ -2,44 +2,41 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import reviewServices from "../../services/reviewServices";
 
 const initialState = {
-    reviews: [],
-    loading: false,
-    errors: undefined,
+	reviews: [],
+	loading: false,
+	errors: undefined,
 };
 
 //thunk
-export const fetchReviews = createAsyncThunk(
-    "reviews/fetchReviews",
-    async () => {
-        try {
-            const reviews = await reviewServices.getAllReviews();
-            return reviews;
-        } catch (error) {
-            return error;
-        }
-    }
-);
+export const fetchReviews = createAsyncThunk("reviews/fetchReviews", async () => {
+	try {
+		const reviews = await reviewServices.getAllReviews();
+		return reviews;
+	} catch (error) {
+		return error;
+	}
+});
 
 export const reviewSlice = createSlice({
-    name: "reviews/fetchReviews",
-    initialState,
-    extraReducers: {
-        [fetchReviews.fulfilled]: (state, action) => {
-            state.reviews = action.payload;
-            state.loading = false;
-            state.errors = undefined;
-        },
-        [fetchReviews.rejected]: (state, action) => {
-            state.reviews = [];
-            state.loading = false;
-            state.errors = action.payload;
-        },
-        [fetchReviews.pending]: (state, action) => {
-            state.reviews = [];
-            state.loading = true;
-            state.errors = undefined;
-        },
-    },
+	name: "reviews/fetchReviews",
+	initialState,
+	extraReducers: {
+		[fetchReviews.fulfilled]: (state, action) => {
+			state.reviews = action.payload;
+			state.loading = false;
+			state.errors = undefined;
+		},
+		[fetchReviews.rejected]: (state, action) => {
+			state.reviews = [];
+			state.loading = false;
+			state.errors = action.payload;
+		},
+		[fetchReviews.pending]: (state, action) => {
+			state.reviews = [];
+			state.loading = true;
+			state.errors = undefined;
+		},
+	},
 });
 
 export const selectAllReviews = (state) => state.reviews.reviews;
