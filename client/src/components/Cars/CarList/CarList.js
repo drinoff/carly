8479bc65f8af3abfb75346ee8e81000d054bf036
Drinoff <drinoff@gmail.com>
2022-04-04 23:44:43 +0200

    cleared unused imports

--- client/src/components/Cars/CarList/CarList.js
+++ client/src/components/Cars/CarList/CarList.js
@@ -1,33 +1,22 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import Car from "./Car/Car";
 import AddCarButton from "../AddCar/AddCarButton";
 import "./CarList.css";
-import {
-    isAuthenticatedSelector,
-    userSelector,
-} from "../../../features/auth/authSlice";
+import { isAuthenticatedSelector, userSelector } from "../../../features/auth/authSlice";
 
 const CarList = ({ cars, carClickHandler }) => {
-    const user = useSelector(userSelector);
-    const isAuthenticated = useSelector(isAuthenticatedSelector);
+	const user = useSelector(userSelector);
+	const isAuthenticated = useSelector(isAuthenticatedSelector);
 
-    return (
-        <div className="CarList-container">
-            {cars.map((car) => (
-                <Car
-                    key={car.brand}
-                    car={car}
-                    carClickHandler={carClickHandler}
-                />
-            ))}
-            {isAuthenticated ? (
-                user.role === "admin" ? (
-                    <AddCarButton />
-                ) : null
-            ) : null}
-        </div>
-    );
+	return (
+		<div className="CarList-container">
+			{cars.map((car) => (
+				<Car key={car.brand} car={car} carClickHandler={carClickHandler} />
+			))}
+			{isAuthenticated ? user.role === "admin" ? <AddCarButton /> : null : null}
+		</div>
+	);
 };
 
 export default CarList;
