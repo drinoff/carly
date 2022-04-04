7b22804c7500e07f105345fe1d75a7f0bf9ef7fa
Drinoff <drinoff@gmail.com>
2022-04-04 23:42:48 +0200

    classifiedController routes and callbacks added

--- api/src/controlers/classifiedsController.js
+++ api/src/controlers/classifiedsController.js
@@ -12,6 +12,46 @@ const getAllClassifieds = (req, res) => {
 		});
 };
 
+const addClassified = (req, res) => {
+	const classified = req.body;
+	classifiedServices
+		.addClassified(classified)
+		.then((classified) => {
+			res.json(classified);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+};
+
+const updateClassified = (req, res) => {
+	const classified = req.body;
+	const classifiedId = req.params.id;
+	classifiedServices
+		.updateClassified(classifiedId, classified)
+		.then((classified) => {
+			res.json(classified);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+};
+
+const deleteClassified = (req, res) => {
+	const id = req.params.id;
+	classifiedServices
+		.deleteClassified(id)
+		.then((classified) => {
+			res.json(classified);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+};
+
 router.get("/", getAllClassifieds);
+router.post("/", addClassified);
+router.delete("/:id", deleteClassified);
+router.put("/:id", updateClassified);
 
 module.exports = router;
