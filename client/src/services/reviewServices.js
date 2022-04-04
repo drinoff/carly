a78a5847e60c281bcb41cee8a8bb66ee617e2e5e
Drinoff <drinoff@gmail.com>
2022-04-04 12:42:09 +0200

    update and delete review services added

--- client/src/services/reviewServices.js
+++ client/src/services/reviewServices.js
@@ -1,7 +1,7 @@
 import { BASE_URL } from "../constants";
 
 const user = JSON.parse(localStorage.getItem("user"));
-const accessToken = user.accessToken;
+const accessToken = user?.accessToken;
 
 const getAllReviews = async () => {
 	return fetch(`${BASE_URL}/reviews`)
@@ -24,9 +24,50 @@ const addReview = (review) => {
 		.catch((err) => err.message);
 };
 
+const updateReview = (reviewId, review) => {
+	return fetch(`${BASE_URL}/reviews/${reviewId}`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+			"x-authorization": accessToken,
+		},
+		body: JSON.stringify(review),
+	})
+		.then((res) => res.json())
+		.catch((err) => err.message);
+};
+
+const deleteReview = (reviewId) => {
+	return fetch(`${BASE_URL}/reviews/${reviewId}`, {
+		method: "DELETE",
+		headers: {
+			"Content-Type": "application/json",
+			"x-authorization": accessToken,
+		},
+	})
+		.then((res) => res.json())
+		.catch((err) => err.message);
+};
+
+const addComment = (reviewId, review) => {
+	return fetch(`${BASE_URL}/reviews/${reviewId}`, {
+		method: "PATCH",
+		headers: {
+			"Content-Type": "application/json",
+			"x-authorization": accessToken,
+		},
+		body: JSON.stringify(review),
+	})
+		.then((res) => res.json())
+		.catch((err) => err.message);
+};
+
 const blogServices = {
 	getAllReviews,
 	addReview,
+	updateReview,
+	deleteReview,
+	addComment,
 };
 
 export default blogServices;
