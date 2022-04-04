77fda137f528486fe2be4c3b56826ce73ab867d2
Drinoff <drinoff@gmail.com>
2022-04-04 12:40:02 +0200

    admin buttons for review section

--- client/src/components/ReviewArticle/ReviewAdminButtons/ReviewAdminButtons.js
+++ client/src/components/ReviewArticle/ReviewAdminButtons/ReviewAdminButtons.js
@@ -1,10 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import reviewServices from "../../../services/reviewServices";
 import "./ReviewAdminButtons.css";
 
-const ReviewAdminButtons = () => {
+const ReviewAdminButtons = ({ review }) => {
+	const navigate = useNavigate();
+
+	const onReviewEditClickHandler = () => {
+		navigate(`/blog/${review._id}/edit`, { state: { review } });
+	};
+	const onReviewDeleteClickHandler = () => {
+		reviewServices
+			.deleteReview(review._id)
+			.then(() => {
+				navigate("/");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 	return (
 		<div className="ReviewAdminButtons">
-			<button className="ReviewAdminButtons-edit">Edit</button>
-			<button className="ReviewAdminButtons-delete">Delete</button>
+			<button className="ReviewAdminButtons-edit" onClick={onReviewEditClickHandler}>
+				Edit
+			</button>
+			<button className="ReviewAdminButtons-delete" onClick={onReviewDeleteClickHandler}>
+				Delete
+			</button>
 		</div>
 	);
 };
