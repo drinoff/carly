1fbce9db7b6fcec5eb8bd0d9aa33dfa8b346f88d
Drinoff <drinoff@gmail.com>
2022-04-04 23:45:11 +0200

    created styled

--- client/src/components/Classified/AddClassified/AddClassified.js
+++ client/src/components/Classified/AddClassified/AddClassified.js
@@ -1,49 +1,77 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
+import { userSelector } from "../../../features/auth/authSlice";
+import Pictures from "./Pictures";
+import TechData from "./TechData";
+import ExtrasCheckboxes from "./ExtrasCheckboxes";
+import Location from "./Location";
+import { INITIAL_CLASSIFIED_STATE, TECH_DATA_INITIAL_STATE, INITIAL_LOCATION_STATE, EXTRAS } from "../../../constants";
+import classifiedServices from "../../../services/classifiedServices";
 
 import "./AddClassified.css";
 
 const AddClassified = () => {
+	const navigate = useNavigate();
+	const [pictures, setPictures] = useState(["", "", "", "", ""]);
+	const [techData, setTechData] = useState(TECH_DATA_INITIAL_STATE);
+	const [extras, setExtras] = useState(EXTRAS);
+	const [location, setLocation] = useState(INITIAL_LOCATION_STATE);
+
+	const [classified, setClassified] = useState(INITIAL_CLASSIFIED_STATE);
+
+	const user = useSelector(userSelector);
+
 	const onAddClassifiedFormSubmitHandler = (event) => {
 		event.preventDefault();
-		const formData = new FormData(event.target);
+		const classifiedData = {
+			...classified,
+			techData: techData,
+			location: location,
+			extras: extras,
+			images: pictures,
+			ownerId: user.id,
+		};
+		classifiedServices
+			.addClassified(classifiedData)
+			.then((res) => {
+				console.log("Classified added successfully: ", res);
+				navigate("/classifieds");
+			})
+			.catch((err) => {
+				console.log("Error adding classified: ", err);
+			});
 	};
 
 	const acordeonHandler = (e) => {
-		if (e.target.textContent === "Fill Car Data") {
-			const panel = document.getElementsByClassName("addArticleContainer")[0];
-			if (panel.style.maxHeight) {
-				panel.style.maxHeight = null;
-			} else {
-				panel.style.maxHeight = panel.scrollHeight * 2 + "px";
-			}
-		} else if (e.target.textContent === "Fill Technical Data") {
-			const panel = document.getElementsByClassName("addArticleTechDataContainer")[0];
-			if (panel.style.maxHeight) {
-				panel.style.maxHeight = null;
-			} else {
-				panel.style.maxHeight = panel.scrollHeight * 2 + "px";
-			}
-		} else if (e.target.textContent === "Fill Extras") {
-			const panel = document.getElementsByClassName("addArticleExtrasContainer")[0];
-			if (panel.style.maxHeight) {
-				panel.style.maxHeight = null;
-			} else {
-				panel.style.maxHeight = panel.scrollHeight * 2 + "px";
-			}
+		const target = e.target;
+		const panel = target.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight * 10 + "px";
 		}
 	};
 
+	const techDataCallBack = (data) => {
+		setTechData(data);
+	};
+
+	const picturesCallBack = (data) => {
+		setPictures(data);
+	};
+
+	const extrasCallBack = (data) => {
+		setExtras(data);
+	};
+
+	const locationCallBack = (data) => {
+		setLocation(data);
+	};
+	console.log(techData);
 	return (
 		<div className="classifiedContainer">
-			<button className="classifiedAccordion articleData" onClick={acordeonHandler}>
-				Fill Car Data
-			</button>
-			<button className="classifiedAccordion techData" onClick={acordeonHandler}>
-				Fill Technical Data
-			</button>
-			<button className="classifiedAccordion extras" onClick={acordeonHandler}>
-				Fill Extras
-			</button>
 			<Box
 				className="addArticleContainer"
 				sx={{
@@ -52,7 +80,7 @@ const AddClassified = () => {
 					width: "100%",
 				}}
 			>
-				<h1 className="addArticleHeading">Car Data</h1>
+				<h1 className="addArticleHeading">Add Classified</h1>
 				<form
 					id="addArticle-form"
 					action="POST"
@@ -61,21 +89,51 @@ const AddClassified = () => {
 				>
 					<div className="titleContainer">
 						<label htmlFor="title">Title</label>
-						<input autoComplete="off" id="title" type="text" name="title" className="formInputStyle" />
+						<input
+							autoComplete="off"
+							id="title"
+							type="text"
+							name="title"
+							className="formInputStyle"
+							value={classified.title}
+							onChange={(e) => setClassified({ ...classified, title: e.target.value })}
+						/>
 					</div>
 
 					<div className="brandContainer">
 						<label htmlFor="brand">Brand</label>
-						<input autoComplete="off" id="brand" type="text" name="brand" className="formInputStyle" />
+						<input
+							autoComplete="off"
+							id="brand"
+							type="text"
+							name="brand"
+							className="formInputStyle"
+							value={classified.brand}
+							onChange={(e) => setClassified({ ...classified, brand: e.target.value })}
+						/>
 					</div>
 
 					<div className="modelContainer">
-						<label htmlFor="logo">Model</label>
-						<input id="model" type="text" name="model" className="formInputStyle" />
+						<label htmlFor="model">Model</label>
+						<input
+							id="model"
+							type="text"
+							name="model"
+							className="formInputStyle"
+							value={classified.model}
+							onChange={(e) => setClassified({ ...classified, model: e.target.value })}
+						/>
 					</div>
 					<div className="priceContainer">
 						<label htmlFor="price">Price</label>
-						<input id="price" type="text" name="price" className="formInputStyle" />
+						<input
+							id="price"
+							type="text"
+							name="price"
+							className="formInputStyle"
+							value={classified.price}
+							onChange={(e) => setClassified({ ...classified, price: e.target.value })}
+						/>
 					</div>
 					<div className="articleDescriptionContainer">
 						<label htmlFor="rePass">Description</label>
@@ -85,30 +143,10 @@ const AddClassified = () => {
 							id="articleDescription"
 							type="text"
 							name="articleDescription"
+							value={classified.description}
+							onChange={(e) => setClassified({ ...classified, description: e.target.value })}
 						></textarea>
 					</div>
-
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-a" type="text" name="modelPic-a" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-b" type="text" name="modelPic-b" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-c" type="text" name="modelPic-c" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-d" type="text" name="modelPic-d" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-e" type="text" name="modelPic-e" className="formInputStyle" />
-					</div>
-
 					<div className="categoryContainer">
 						<label htmlFor="category">Category</label>
 						<input
@@ -117,166 +155,36 @@ const AddClassified = () => {
 							type="text"
 							name="category"
 							className="formInputStyle"
+							value={classified.category}
+							onChange={(e) => setClassified({ ...classified, category: e.target.value })}
 						/>
 					</div>
 
-					<input className="addArticleActionButton" type="submit" value="Add Classified" />
-				</form>
-			</Box>
-
-			<Box
-				className="addArticleTechDataContainer"
-				sx={{
-					bgcolor: "#111827",
-					height: "auto",
-					width: "100%",
-				}}
-			>
-				<h1 className="addArticleHeading">Technical Data</h1>
-				<form
-					id="addArticle-form"
-					action="POST"
-					className="addArticle-form"
-					onSubmit={onAddClassifiedFormSubmitHandler}
-				>
-					<div className="titleContainer">
-						<label htmlFor="title">Title</label>
-						<input autoComplete="off" id="title" type="text" name="title" className="formInputStyle" />
+					<button type="button" className="classifiedAccordion accordion" onClick={acordeonHandler}>
+						Add Pictures
+					</button>
+					<div className="picturePanel">
+						<Pictures initialPictures={pictures} picturesCallBack={picturesCallBack} />
 					</div>
 
-					<div className="brandContainer">
-						<label htmlFor="brand">Brand</label>
-						<input autoComplete="off" id="brand" type="text" name="brand" className="formInputStyle" />
+					<button type="button" className="techDataAccordion accordion" onClick={acordeonHandler}>
+						Technical Data
+					</button>
+					<div className="picturePanel techDataContainer">
+						<TechData techDataCallback={techDataCallBack} techData={techData} />
 					</div>
 
-					<div className="modelContainer">
-						<label htmlFor="logo">Model</label>
-						<input id="model" type="text" name="model" className="formInputStyle" />
+					<button type="button" className="extrasdAccordion accordion" onClick={acordeonHandler}>
+						Extras
+					</button>
+					<div className="picturePanel">
+						<ExtrasCheckboxes extrasCallBack={extrasCallBack} />
 					</div>
-					<div className="priceContainer">
-						<label htmlFor="price">Price</label>
-						<input id="price" type="text" name="price" className="formInputStyle" />
-					</div>
-					<div className="articleDescriptionContainer">
-						<label htmlFor="rePass">Description</label>
-						<textarea
-							rows={6}
-							cols={50}
-							id="articleDescription"
-							type="text"
-							name="articleDescription"
-						></textarea>
-					</div>
-
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-a" type="text" name="modelPic-a" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-b" type="text" name="modelPic-b" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-c" type="text" name="modelPic-c" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-d" type="text" name="modelPic-d" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-e" type="text" name="modelPic-e" className="formInputStyle" />
-					</div>
-
-					<div className="categoryContainer">
-						<label htmlFor="category">Category</label>
-						<input
-							autoComplete="off"
-							id="category"
-							type="text"
-							name="category"
-							className="formInputStyle"
-						/>
-					</div>
-
-					<input className="addArticleActionButton" type="submit" value="Add Classified" />
-				</form>
-			</Box>
-			<Box
-				className="addArticleExtrasContainer"
-				sx={{
-					bgcolor: "#111827",
-					height: "auto",
-					width: "100%",
-				}}
-			>
-				<h1 className="addArticleHeading">Extras</h1>
-				<form
-					id="addArticle-form"
-					action="POST"
-					className="addArticle-form"
-					onSubmit={onAddClassifiedFormSubmitHandler}
-				>
-					<div className="titleContainer">
-						<label htmlFor="title">Title</label>
-						<input autoComplete="off" id="title" type="text" name="title" className="formInputStyle" />
-					</div>
-
-					<div className="brandContainer">
-						<label htmlFor="brand">Brand</label>
-						<input autoComplete="off" id="brand" type="text" name="brand" className="formInputStyle" />
-					</div>
-
-					<div className="modelContainer">
-						<label htmlFor="logo">Model</label>
-						<input id="model" type="text" name="model" className="formInputStyle" />
-					</div>
-					<div className="priceContainer">
-						<label htmlFor="price">Price</label>
-						<input id="price" type="text" name="price" className="formInputStyle" />
-					</div>
-					<div className="articleDescriptionContainer">
-						<label htmlFor="rePass">Description</label>
-						<textarea
-							rows={6}
-							cols={50}
-							id="articleDescription"
-							type="text"
-							name="articleDescription"
-						></textarea>
-					</div>
-
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-a" type="text" name="modelPic-a" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-b" type="text" name="modelPic-b" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-c" type="text" name="modelPic-c" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-d" type="text" name="modelPic-d" className="formInputStyle" />
-					</div>
-					<div className="pictureContainer">
-						<label htmlFor="modelPic">Image URL</label>
-						<input id="modelPic-e" type="text" name="modelPic-e" className="formInputStyle" />
-					</div>
-
-					<div className="categoryContainer">
-						<label htmlFor="category">Category</label>
-						<input
-							autoComplete="off"
-							id="category"
-							type="text"
-							name="category"
-							className="formInputStyle"
-						/>
+					<button type="button" className="locationAccordeon accordion" onClick={acordeonHandler}>
+						Location
+					</button>
+					<div className="picturePanel locationContainer">
+						<Location initialLocation={location} locationCallBack={locationCallBack} />
 					</div>
 
 					<input className="addArticleActionButton" type="submit" value="Add Classified" />
