953818774ee9b5647f817e8acba86fd9f7c8ed34
Drinoff <drinoff@gmail.com>
2022-04-04 12:40:30 +0200

    Route guard unified

--- client/src/components/RouteGuard/RouteGuard.js
+++ client/src/components/RouteGuard/RouteGuard.js
@@ -1,16 +1,36 @@
-import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 
 const RouteGuard = ({ children }) => {
-    const onError = (error) => {
-        console.log(error);
-    };
-    const user = JSON.parse(localStorage.getItem("user"));
-    useEffect(() => {
-        if (user.role !== "admin") {
-            onError("You need to Login or Register to be able acces this page");
-        }
-    }, [user.role]);
-    return user.role === "admin" ? children : <Navigate to="/login" />;
+	const onError = (error) => {
+		console.log(error);
+	};
+	const user = JSON.parse(localStorage.getItem("user"));
+
+	function result() {
+		if (children.type.name === "AddArticle" || children.type.name === "EditArticle") {
+			if (!user?.role) {
+				onError("You are not authorized to add or edit articles");
+				return <Navigate to="/login" />;
+			} else if (user.role === "user") {
+				onError("You are not authorized to add or edit articles");
+				return <Navigate to="/" />;
+			} else {
+				return children;
+			}
+		} else if (
+			children.type.name === "AddCar" ||
+			children.type.name === "EditCar" ||
+			children.type.name === "AddModel" ||
+			children.type.name === "EditModel"
+		) {
+			if (user?.role !== "admin") {
+				onError("You are not authorized to add or edit cars");
+				return <Navigate to="/" />;
+			} else {
+				return children;
+			}
+		}
+	}
+	return result();
 };
 export default RouteGuard;
