176d85e52407d75bc8d21c183aee0b028319db1d
Drinoff <drinoff@gmail.com>
2022-04-04 12:36:05 +0200

    added update review and addcomment service

--- api/src/controlers/reviewsController.js
+++ api/src/controlers/reviewsController.js
@@ -8,10 +8,10 @@ const getAllReviews = (req, res) => {
 	reviewServices
 		.getAllReviews()
 		.then((reviews) => {
-			res.send(reviews);
+			res.json(reviews);
 		})
 		.catch((err) => {
-			res.send(err);
+			res.json(err);
 		});
 };
 
@@ -26,7 +26,56 @@ const addReview = (req, res) => {
 		});
 };
 
+const updateReview = (req, res) => {
+	reviewServices
+		.updateReview(req.params.reviewId, req.body)
+		.then((updatedReview) => {
+			res.json({
+				message: "Review updated successfully",
+				updatedReview,
+			});
+		})
+		.catch((err) => {
+			res.json({
+				message: "Error updating review",
+				error: err,
+			});
+		});
+};
+
+const deleteReview = (req, res) => {
+	reviewServices
+		.deleteReview(req.params.reviewId)
+		.then((review) => {
+			res.json(review);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+};
+
+const addComment = (req, res) => {
+	reviewServices
+		.addComment(req.params.reviewId, req.body)
+
+		.then((updatedReview) => {
+			res.json({
+				message: "Comment added successfully",
+				updatedReview,
+			});
+		})
+		.catch((err) => {
+			res.json({
+				message: "Error adding comment",
+				error: err,
+			});
+		});
+};
+
 router.get("/", getAllReviews);
 router.post("/", isAuthorized(), isReviewer(), validate(schemas.reviewSchema), addReview);
+router.put("/:reviewId", isAuthorized(), isReviewer(), validate(schemas.reviewSchema), updateReview);
+router.delete("/:reviewId", isAuthorized(), isReviewer(), deleteReview);
+router.patch("/:reviewId", isAuthorized(), addComment);
 
 module.exports = router;
