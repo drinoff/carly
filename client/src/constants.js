db96fc8ef5478e3096cc12e7d6463ce106d5d510
Drinoff <drinoff@gmail.com>
2022-04-04 23:47:21 +0200

    initialState constats created

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,1 +1,69 @@
 export const BASE_URL = "http://localhost:5000";
+export const EXTRAS = [
+	"Alarm",
+	"Alloy wheels",
+	"Anti-lock Brake System (ABS)",
+	"Audio",
+	"remote control",
+	"Bluetooth",
+	"Climate control",
+	"Digital radio",
+	"Electric door mirrors",
+	"Electric sunroof",
+	"Electrically adjustable drivers seat",
+	"Electrically adjustable passenger seat",
+	"Front electric windows",
+	"Front side airbags",
+	"Heated door mirrors",
+	"Heated front seat",
+	"Height adjustable drivers seat",
+	"Immobiliser",
+	"Isofix child seat anchor points",
+	"Navigation system",
+	"Passenger airbag",
+	"Power Assisted Steering",
+	"Rear electric windows",
+	"Rear park assist camera",
+	"Remote central locking",
+	"Reverse parking aid",
+	"Service interval indicator",
+	"Traction control",
+	"Trip computer",
+	"USB input",
+	"HUD",
+];
+
+export const INITIAL_CLASSIFIED_STATE = {
+	title: "",
+	brand: "",
+	model: "",
+	price: "",
+	description: "",
+	category: "",
+};
+
+export const TECH_DATA_INITIAL_STATE = {
+	year: "",
+	milleage: "",
+	engine: "",
+	transmission: "",
+	color: "",
+	doors: "",
+	fuel: "",
+	drive: "",
+	body: "",
+	interior: "",
+	hp: "",
+	consumption: "",
+	euroClass: "",
+};
+
+export const INITIAL_LOCATION_STATE = {
+	city: "",
+	country: "",
+	street: "",
+	zip: "",
+	number: "",
+};
+
+export const sliderWidth = 650;
