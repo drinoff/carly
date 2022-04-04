d3ea1add67c6e0bfd0050bd7b46b82d05021b52a
Drinoff <drinoff@gmail.com>
2022-04-04 23:43:17 +0200

    Classified Model bugs cleared

--- api/src/models/Classified.js
+++ api/src/models/Classified.js
@@ -1,91 +1,105 @@
 const mongoose = require("mongoose");
 
 const classifiedSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    brand: {
-        type: String,
-        required: true,
-    },
-    model: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
+	title: {
+		type: String,
+		required: true,
+	},
+	brand: {
+		type: String,
+		required: true,
+	},
+	model: {
+		type: String,
+		required: true,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	description: {
+		type: String,
+	},
 
-    images: [
-        {
-            type: String,
-            required: true,
-        },
-    ],
-    ownerId: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    techData: {
-        year: {
-            type: Number,
-            required: true,
-        },
+	images: [
+		{
+			type: String,
+		},
+	],
+	ownerId: {
+		type: mongoose.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	category: {
+		type: String,
+	},
+	techData: {
+		year: {
+			type: String,
+		},
 
-        hp: {
-            type: Number,
-            required: true,
-        },
-        category: {
-            type: String,
-            required: true,
-        },
-        milleage: {
-            type: Number,
-            required: true,
-        },
-        transmission: {
-            type: String,
-            required: true,
-        },
-        engineCapacity: {
-            type: Number,
-            required: true,
-        },
-        doors: {
-            type: Number,
-            required: true,
-        },
-        fuel: {
-            type: String,
-            required: true,
-        },
-        color: {
-            type: String,
-            required: true,
-        },
-        interiorColor: {
-            type: String,
-            required: true,
-        },
-        extras: [
-            {
-                type: String,
-                required: true,
-            },
-        ],
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now,
-    },
+		hp: {
+			type: String,
+		},
+
+		milleage: {
+			type: String,
+		},
+		transmission: {
+			type: String,
+		},
+		engine: {
+			type: String,
+		},
+		doors: {
+			type: String,
+		},
+		drive: {
+			type: String,
+		},
+		fuel: {
+			type: String,
+		},
+		color: {
+			type: String,
+		},
+		interior: {
+			type: String,
+		},
+		consumption: {
+			type: String,
+		},
+		euroClass: {
+			type: String,
+		},
+	},
+	extras: [
+		{
+			type: String,
+		},
+	],
+	location: {
+		street: {
+			type: String,
+		},
+		number: {
+			type: String,
+		},
+		zip: {
+			type: String,
+		},
+		city: {
+			type: String,
+		},
+		country: {
+			type: String,
+		},
+	},
+	createdDate: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
 const Classified = mongoose.model("Classified", classifiedSchema);
