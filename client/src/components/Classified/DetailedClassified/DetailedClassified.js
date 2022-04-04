e6151f9ea51278ca82cec62af7c21bc21518cc99
Drinoff <drinoff@gmail.com>
2022-04-04 23:46:23 +0200

    extras added styled

--- client/src/components/Classified/DetailedClassified/DetailedClassified.js
+++ client/src/components/Classified/DetailedClassified/DetailedClassified.js
@@ -3,58 +3,71 @@ import { useLocation } from "react-router-dom";
 import { Box } from "@mui/material";
 import TechnicalData from "./TechnicalData/TechnicalData";
 import Slider from "../Slider/Slider";
+import Extras from "./Extras/Extras";
+import ClassifiedAdminButtons from "./ClassifiedAdminButtons/ClassifiedAdminButtons";
+
+import { sliderWidth } from "../../../constants";
 import "./DetailedClassified.css";
 
-const sliderWidth = 650;
-
 const DetailedClassified = () => {
-    const location = useLocation();
-    const { classified } = location.state;
-    console.log(classified);
-    return (
-        <div className="DetailedClassified-container">
-            <div className="image-techData-Container">
-                <Box
-                    className="DetailedClassified-image-Container"
-                    sx={{
-                        bgcolor: "#111827",
-                        height: "510px",
-                        width: "44%",
-                    }}
-                >
-                    <Slider
-                        images={classified.images}
-                        width={sliderWidth}
-                        className="DetailedClassified-slider"
-                    />
-                </Box>
-                <Box
-                    className="DetailedClassified-techData-Container"
-                    sx={{
-                        bgcolor: "#111827",
-                        height: "510px",
-                        width: "44%",
-                    }}
-                >
-                    <TechnicalData classified={classified} />
-                </Box>
-            </div>
-            <div>
-                <Box
-                    className="DetailedClassified-description-Container"
-                    sx={{
-                        bgcolor: "#111827",
-                        height: "auto",
-                        width: "70%",
-                    }}
-                >
-                    <div className="DetailedClassified-description">
-                        <p>{classified.description}</p>
-                    </div>
-                </Box>
-            </div>
-        </div>
-    );
+	const location = useLocation();
+	const { classified } = location.state;
+
+	return (
+		<div className="DetailedClassified-container">
+			<div className="image-techData-Container">
+				<Box
+					className="DetailedClassified-image-Container"
+					sx={{
+						bgcolor: "#111827",
+						height: "510px",
+						width: "44%",
+					}}
+				>
+					<Slider images={classified.images} width={sliderWidth} className="DetailedClassified-slider" />
+				</Box>
+				<Box
+					className="DetailedClassified-techData-Container"
+					sx={{
+						bgcolor: "#111827",
+						height: "510px",
+						width: "44%",
+					}}
+				>
+					<TechnicalData classified={classified} />
+				</Box>
+			</div>
+			<div>
+				<Box
+					className="DetailedClassified-description-Container"
+					sx={{
+						bgcolor: "#111827",
+						height: "auto",
+						width: "70%",
+					}}
+				>
+					<div className="DetailedClassified-description">
+						<p>{classified.description}</p>
+					</div>
+				</Box>
+				<Box
+					className="DetailedClassified-description-Container"
+					sx={{
+						bgcolor: "#111827",
+						height: "auto",
+						width: "70%",
+					}}
+				>
+					<div className="DetailedClassified-extras">
+						{classified.extras.map((extra) => (
+							<Extras key={extra} extra={extra} />
+						))}
+					</div>
+				</Box>
+			</div>
+			<ClassifiedAdminButtons classified={classified} />
+		</div>
+	);
 };
 
 export default DetailedClassified;
