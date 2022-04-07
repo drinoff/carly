f81f8ea9454832942502c07453da4f35ed8764dd
Drinoff <drinoff@gmail.com>
2022-04-07 22:01:52 +0200

    added clock options

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -3,3 +3,18 @@ export const getAccessToken = () => {
 	const accessToken = user?.accessToken;
 	return accessToken;
 };
+
+export const clockOptions = {
+	useCustomTime: false,
+	width: "149px",
+	border: false,
+	borderColor: "#10b981",
+	baseColor: "#0b0f19",
+	centerColor: "#0b0f19",
+	centerBorderColor: "#10b981",
+	handColors: {
+		second: "#d81c7a",
+		minute: "#7582eb",
+		hour: "#7582eb",
+	},
+};
