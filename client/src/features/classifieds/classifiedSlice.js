a7de92a2b8873295b61eb0afc6493ed7d95b7b6a
Drinoff <drinoff@gmail.com>
2022-04-09 12:27:15 +0200

    bug cleared

--- client/src/features/classifieds/classifiedSlice.js
+++ client/src/features/classifieds/classifiedSlice.js
@@ -2,35 +2,32 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import classifiedServices from "../../services/classifiedServices";
 
 const initialState = {
-    classifieds: [],
+	classifieds: [],
 };
 
-export const fetchClassifieds = createAsyncThunk(
-    "classifieds/fetchClassifieds",
-    async () => {
-        try {
-            const classifieds = await classifiedServices.getAllClassifieds();
-            return classifieds;
-        } catch (error) {
-            return error;
-        }
-    }
-);
+export const fetchClassifieds = createAsyncThunk("classifieds/fetchClassifieds", async () => {
+	try {
+		const classifieds = await classifiedServices.getAllClassifieds();
+		return classifieds;
+	} catch (error) {
+		return error;
+	}
+});
 
 export const classifiedSlice = createSlice({
-    name: "classifieds/fetchClassifieds",
-    initialState,
-    extraReducers: {
-        [fetchClassifieds.fulfilled]: (state, action) => {
-            state.classifieds = action.payload;
-        },
-        [fetchClassifieds.rejected]: (state, action) => {
-            state.classifieds = [];
-        },
-        [fetchClassifieds.pending]: (state, action) => {
-            state.classifieds = [];
-        },
-    },
+	name: "classifieds/fetchClassifieds",
+	initialState,
+	extraReducers: {
+		[fetchClassifieds.fulfilled]: (state, action) => {
+			state.classifieds = action.payload;
+		},
+		[fetchClassifieds.rejected]: (state, action) => {
+			state.classifieds = [];
+		},
+		[fetchClassifieds.pending]: (state, action) => {
+			state.classifieds = [];
+		},
+	},
 });
 
 export const selectAllClassifieds = (state) => state.classifieds.classifieds;
