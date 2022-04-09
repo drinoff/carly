832e0cb34a099213baf68cff66c47e73d3647f75
Drinoff <drinoff@gmail.com>
2022-04-09 12:25:22 +0200

    sort functionality

---
+++ client/src/components/Classified/SortClassifieds/SortClassifieds.js
@@ -0,0 +1,5 @@
+const SortClassifieds = ()=>{
+    return ()
+}
+
+export default SortClassifieds;
--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -18,3 +18,39 @@ export const clockOptions = {
 		hour: "#7582eb",
 	},
 };
+
+export const handleFiltration = (filter, classifieds) => {
+	return classifieds.filter((classified) => {
+		return (
+			classified.title.toLowerCase().includes(filter.toLowerCase()) ||
+			classified.brand.toLowerCase().includes(filter.toLowerCase()) ||
+			classified.model.toLowerCase().includes(filter.toLowerCase()) ||
+			classified.category.toLowerCase().includes(filter.toLowerCase()) ||
+			classified.description.toLowerCase().includes(filter.toLowerCase()) ||
+			classified.location.city.toLowerCase().includes(filter.toLowerCase()) ||
+			classified.techData.fuel.toLowerCase().includes(filter.toLowerCase()) ||
+			classified.techData.transmission.toLowerCase().includes(filter.toLowerCase()) ||
+			classified.techData.interior.toLowerCase().includes(filter.toLowerCase()) ||
+			classified.techData.color.toLowerCase().includes(filter.toLowerCase())
+		);
+	});
+};
+
+export const handleSort = (sort, classifieds) => {
+	switch (sort) {
+		case "Price Ascending":
+			return classifieds.slice().sort((a, b) => a.price - b.price);
+		case "Price Descending":
+			return classifieds.slice().sort((a, b) => b.price - a.price);
+		case "HP Ascending":
+			return classifieds.slice().sort((a, b) => a.techData.hp - b.techData.hp);
+		case "HP Descending":
+			return classifieds.slice().sort((a, b) => b.techData.hp - a.techData.hp);
+		case "Milleage Ascending":
+			return classifieds.slice().sort((a, b) => a.techData.milleage - b.techData.milleage);
+		case "Milleage Descending":
+			return classifieds.slice().sort((a, b) => b.techData.milleage - a.techData.milleage);
+		default:
+			return classifieds;
+	}
+};
